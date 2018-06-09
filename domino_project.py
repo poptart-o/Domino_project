2aabdebf7083f28712188ac048aaece52ef72c9f
Jakoc Rivas <Sam_Rivas_Sime@outlook.com>
2018-06-09 13:17:33 -0400

    add class Ficha
    class que tiene una ficha de domino con sus dos valores con un method
    para mostrar esos valores

--- domino_project.py
+++ domino_project.py
@@ -1,1 +1,14 @@
 import random
+
+
+class Ficha(object):
+
+    def __init__(self, value1, value2):
+        self.value1 = value1
+        self.value2 = value2
+
+    def mostrar_ficha(self):
+        print '''
+         {}
+        ---
+         {}'''.format(self.value1, self.value2)
