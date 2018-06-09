078e739d8f79f60586ee349144d8c97a876b63b6
Jakoc Rivas <Sam_Rivas_Sime@outlook.com>
2018-06-09 13:21:21 -0400

    add class Caja
    class de una caja de domino con una list que se usa como caja para
    guardar las fichas con los methods:
    crear(): para poner las 28 ficahs en la caja;
    mostrar(): muestra todas las fichas de la caja;
    barajar(): baraja todas las fichas de la caja aleatoriamente;
    tomar(): toma una ficha de la caja(ya sea barajada o no).

--- domino_project.py
+++ domino_project.py
@@ -12,3 +12,29 @@ class Ficha(object):
          {}
         ---
          {}'''.format(self.value1, self.value2)
+
+
+class Caja(object):
+
+    def __init__(self):
+
+        self.caja = []
+
+    def crear(self):
+        for valor1 in range(0, 7):
+            for valor2 in range(valor1, 7):
+                self.caja.append(Ficha(valor1, valor2))
+
+    def mostrar(self):
+        if len(self.caja) == 0:
+            print 'No hay fichas en la caja'
+        for ficha in self.caja:
+            print [ficha.value1, ficha.value2]
+
+    def barajar(self):
+        for i in range(1, len(self.caja) - 1, -1):
+            r = random.randint(0, i)
+            self.caja[i], self.caja[r] = self.caja[r], self.caja[i]
+
+    def tomar_ficha(self):
+        return self.caja.pop()
